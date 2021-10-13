5d95ed188d63027ebce0a9e894ec8debc604466f
Andrei Kargu <andrei.kargu@suomi24.fi>
2021-10-13 13:04:32 +0300

    1.6

--- tehtava_1.5.js
+++ tehtava_1.5.js
@@ -1,11 +1,11 @@
-states_dictionary={ 
-    1:["Maanantai"], 
-    2:["Tiistai"], 
-    3:["Keskiviikko"]
-    4:["Torstai"], 
-    5:["Perjantai"], 
-    6:["Lauantai"],
-    7:["Sunnuntai"]
+sanakirja={ 
+    1: "Maanantai", 
+    2: "Tiistai", 
+    3: "Keskiviikko",
+    4: "Torstai", 
+    5: "Perjantai", 
+    6: "Lauantai",
+    7: "Sunnuntai"
 };
 
 var readline = require('readline');
@@ -16,12 +16,6 @@ var luku = readline.createInterface({
   });
 
 luku.question("Anna viikonpäivän numeron: ",function(answer) { 
-    a = answer
-    if (a > 100) {
-        console.log("Syötit luvun, joka on suurempi kuin 100");
-    }   else if (a < 100) {
-        console.log("Syötit luvun, joka on pienempi kuin 100");
-    }  else {
-        console.log("Luku on 100");
-    }
+    console.log(sanakirja[answer]);
+    
 });   
